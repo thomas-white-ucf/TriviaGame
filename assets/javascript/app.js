b5b24fbfcb4b2e7308af7282e745223db9f24ed6
Thomas White <thomas.white.ucf@gmail.com>
2019-07-09 23:17:29 -0400

    submit HW-JQ-2 - deadpool trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,18 @@
-// +_ STEPS TO COMPLETE EASY HOMEWORK ASSIGNMENT
-
-//  --- 1.   Create a start button, this button - starts timer and populates screen with timer, questions and answers buttons)
-
-//  --- 2.   Make Timer that begins on button click, and that shows it counting down on the screen
-
-//  --- 3.   Make a set of questions that appear on the screen at button click
-
-//  --- 4.   Make a set of selectable answers appear on screen
-//                    - only allow one answer at a time
-
-//  --- 5.   Keep track of answers, compare them to correct answers
-
-//  --- 6.   when timer is up show user how many questions they got correct or wrong
-
-//  --- 7.
-// ___________________________
 
 var intervalId;
 var number = 30;
 var clockRunning;
 let answersCorrect = 0;
+let questionNumber = 0;
 let answersWrong = 0;
 var numberOfQuestions = 4;
+
+
+var questionList = ['Is Deadpool from Marvel or DC comic books?', 'Did Thanos do anything wrong?',
+    'How many Marvel movies were before Avengers End Game?', 'When was the first Ironman Movie Released?'];
+var answerList1 = ['DC Comics', 'I am Groot', 'Less than 20', 'May 2, 2008'];
+var answerList2 = ['Marvel Studios', 'Reddit said no', 'More than 20!', 'HULK SMASH!'];
+var keyList = ['Marvel Studios', 'I am Groot', 'More than 20!', 'May 2, 2008']
 
 // This code will run as soon as the page loads
 window.onload = function () {
@@ -42,97 +33,94 @@ function start() {
     }
 
     displayQuestions()
-
 }
 
 function displayQuestions() {
+    $("#questionArea").html(questionList[questionNumber]);
+    $("#answersArea1").html(answerList1[questionNumber]);
+    $("#answersArea2").html(answerList2[questionNumber]);
 
-    $("#questionArea").html("Was Deadpool in Avengers End Game?")
-    $("#answersArea1").html("Yes Silly")
-    $("#answersArea2").html("No duhhh im Deadpool")
+}
 
-    $("#answersArea1").click(recordWrong)
-    $("#answersArea1").click(loadQuestion2)
-    $("#answersArea2").click(() => {
+
+$("#answersArea1").click((e) => {
+    // console.log('the text here', $(e.target).text());
+    if ($(e.target).text() === keyList[questionNumber]) {
+        console.log('this is True');
         recordCorrect();
-        loadQuestion2();
-    });
-    // $("#answersArea2").click(loadQuestion2)
-}
-
-function loadQuestion2() {
-    console.log('anser corect here', answersCorrect);
-    $("#questionArea").html("Did Thanos do anything wrong?")
-    $("#answersArea1").html("Reddit said no")
-    $("#answersArea2").html("I am Groot")
-
-    $("#answersArea1").click(recordWrong)
-    $("#answersArea1").click(loadQuestion3)
-    $("#answersArea2").click(() => {
-       // recordCorrect();
-        loadQuestion3();
-    })
-    //$("#answersArea2").click(loadQuestion3)
-
-}
+    } else {
+        console.log('this is False');
+        recordWrong();
+    }
+});
 
 
-function loadQuestion3() {
-    $("#questionArea").html("How many Marvel movies were before Avengers End Game?")
-    $("#answersArea1").html("Less than 20")
-    $("#answersArea2").html("More than 20 movies Wow!")
-
-    $("#answersArea1").click(recordWrong)
-    $("#answersArea1").click(loadQuestion4)
-    $("#answersArea2").click(() => {
-       // recordCorrect();
-        loadQuestion4();
-    })
-   //$("#answersArea2").click(loadQuestion4)
-
-}
-
-
-function loadQuestion4() {
-    $("#questionArea").html("When did the first Ironman Movie Released?")
-    $("#answersArea2").html("May 2, 2008")
-    $("#answersArea1").html("HULK SMASH!")
-
-    $("#answersArea2").click(() => {
-        //recordCorrect();
-         endGame();
-     })
-    //$("#answersArea1").click(endGame)
-    // $("#answersArea2").click(() => {
-    //     //recordCorrect();
-    //     endGame();
-    // })
-    //$("#answersArea2").click(endGame)
-
-}
-
-function endGame() {
-    $("#questionArea").html("")
-    $("#answersArea1").html("")
-    $("#answersArea2").html("")
-
-    clearInterval(intervalId);
-    stop()
-    number = 0
-
-}
+$("#answersArea2").click((e) => {
+    if ($(e.target).text() === keyList[questionNumber]) {
+        console.log('this is True');
+        recordCorrect();
+    } else {
+        console.log('this is False');
+        recordWrong();
+    }
+});
 
 
 // RECORD CORRECT / INCORRECT ANSWERS
 function recordCorrect() {
-    answersCorrect++
-    console.log("Correct answers_" + answersCorrect)
+    answersCorrect++;
+    console.log(answersCorrect)
+    questionNumber++;
+
+    if (questionNumber > questionList.length) {
+
+        // TODO~~~~~~~~~~FIX HOW THESE ARE DISPLAYED..
+        // also Question 3 is not diplaying well
+
+        $("#answersArea1").html("Answers Correct" + answersCorrect)
+        $("#answersArea2").html("Answers Wrong" + answersWrong)
+
+        endGame();
+    } // if last question has been answered, end the game!
+    else if (questionNumber >= 4) {
+        endGame()
+    }
+    else {
+        displayQuestions();
+    }
 }
 function recordWrong() {
-    answersWrong++
-    console.log("Wrong answers_" + answersWrong)
+    answersWrong++;
+    console.log(answersWrong)
+    questionNumber++;
+
+    if (questionNumber > questionList.length) {
+        $("#questionArea").html("Answers Correct " + answersCorrect)
+        $("#questionArea").append("Answers Wrong " + answersWrong)
+
+        endGame();
+
+    } // if last question has been answered, end the game!
+    else if (questionNumber >= 4) {
+        endGame()
+    } else {
+        displayQuestions();
+    }
 }
 
+function endGame() {
+    // DISPLAY CORRECT AND INCORRECT ANSWERS
+
+    $("#questionArea").html("wrong answers" + answersWrong)
+    $("#questionArea").append("right answers" + answersCorrect)
+
+    $("#answersArea1").html("")
+    $("#answersArea2").html("")
+
+    clearInterval(intervalId);
+    number = 0
+
+}
 
 function run() {
     clearInterval(intervalId);
@@ -142,26 +130,18 @@ function run() {
 function decrement() {
 
     number--;
-    //  Decrease number by one.
+    //  Decrease counting-number by one.
     //  Show the number in the #start button tag.
     $("#start").html("<h3>" + "Time Remaining " + number + "</h3>");
 
-
+    //  Once number hits zero... ...run the endGame function.
     if (number === 0) {
-        //  Once number hits zero...
-        //  ...run the stop function.
-        stop();
-        clockRunning = false
-        //  Alert the user that time is up.
-        // alert("Time Up!");
+
+        endGame();
+
     }
 }
 
-function stop() {
-    //  Clears our intervalId
-    //  We just pass the name of the interval -> to the clearInterval function.
-    clearInterval(intervalId);
-}
 
 
 
